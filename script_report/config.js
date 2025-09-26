705026ba6ae4bdd76d0f03eecacdcdd9e4b21741
DatSuperrrrr <137261905+DatSuperrrrr@users.noreply.github.com>
2025-09-27 02:38:34 +0700

    des

--- script_report/config.js
+++ script_report/config.js
@@ -1,4 +1,4 @@
 const apiKey = 'AIzaSyBCF4lFlZFecrRPCZPCoPJRaUB3ZPGK2kk';
 const spreadsheetId = '18ehCo3v-QeAyjEKump-ZffgsX2d2_Q2bljUaGvJvIK4';
 const range = 'sheet2!A1:D';
-const scriptURL = 'https://script.google.com/macros/s/AKfycbz1xNyoA9dHYtrICtjyP6oDOgzja53lLy_4SnOvIaLHD0XzHsf7wyZCi92WR19ONb18/exec';
+const scriptURL = 'https://script.google.com/macros/s/AKfycbz4u8g0sbn5Kf_8yhG8I51WF-SwhTJ6RI3wK-R7cGQBx0OgBaLNHiozsRoz9zkSNzWO/exec';
